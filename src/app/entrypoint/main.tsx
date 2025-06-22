25e73bcf1858e12c57ff0287a88a7d9714fbaad5
Alyona Likhvar <likhvar.a@lexicom.ai>
2025-06-22 20:03:42 +0300

    fix: bugs

--- src/app/entrypoint/main.tsx
+++ src/app/entrypoint/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import { router } from '@/app/routes/routers'
-import '@/styles/base.css'
+import '../styles/base.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
